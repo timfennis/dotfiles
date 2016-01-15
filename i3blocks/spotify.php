7a5620c6763cb7db6924cc7bd2fba6214a324569
Tim Fennis <tim@isset.nl>
2016-01-15 12:59:47 +0100

    spotify and no gaps

---
+++ i3blocks/spotify.php
@@ -0,0 +1,27 @@
+<?php
+
+$output = [];
+
+exec(
+	"dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'",
+	$output
+);
+
+$output = implode('', $output);
+// $output = preg_replace('/\s+/', '', $output);
+
+//xesam:artist"variantarray[string"Muffler"
+//xesam:title"variantstring"Rock"
+
+$matches = [];
+
+// var_dump($output);
+
+preg_match("#xesam:artist\"\s*variant\s*array\s*\[\s*string\s*\"([^\"]*)\"#", $output, $matches);
+$artist = $matches[1];
+
+preg_match("#xesam:title\"\s*variant\s*string\s*\"([^\"]*)\"#", $output, $matches);
+$title = $matches[1];
+
+echo "$artist - $title";
+// var_dump($output);
