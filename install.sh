04a31cd809c5a021d537c82176fa7b806de9bde6
Tim Fennis <tim.fennis@nos.nl>
2023-05-11 10:44:46 +0200

    Store Xresources file

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd "$HOME"
 ln -sf ~/.dotfiles/.zshrc .zshrc
 ln -sf ~/.dotfiles/.vimrc .vimrc
 ln -sf ~/.dotfiles/.vimrc .ideavimrc
+ln -sf ~/.dotfiles/.Xresources .Xresources
 
 mkdir -p .config/i3/ && \
     ln -sf ~/.dotfiles/i3/config .config/i3/config
