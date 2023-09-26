b485dc5e37a73fa9f742c765679d86c435fd4143
Tim Fennis <fennis.tim@gmail.com>
2023-09-26 10:17:54 +0200

    Fish

--- install.sh
+++ install.sh
@@ -7,28 +7,6 @@ red_bold="\e[1;31m"
 bold="\e[1m"
 reset="\e[0m"
 
-# First run some checks and see if the programs that we rely on are installed
-programs=("zsh" "nvim" "alacritty" "i3" "polybar" "fzf")
-packages=("ttf-font-awesome")
-
-for program in "${programs[@]}"; do
-    if command -v "$program" >/dev/null 2>&1; then
-        echo -e "${green_bold}${program}${reset} is installed."
-    else
-        echo -e "${red_bold}${program}${reset} is not installed.\nMake sure to install ${program} before you run the script"
-        exit 1
-    fi
-done
-
-for package in "${packages[@]}"; do
-    if pacman -Qs "$package" >/dev/null 2>&1; then
-        echo -e "${green_bold}${package}${reset} is installed."
-    else
-        echo -e "${red_bold}${package}${reset} is not installed.\nMake sure to install ${package} before you run the script"
-        exit 1
-    fi
-done
-
 # Switch to our user's $HOME directory as a base for creating the various symlinks
 echo -e "Switching to ${bold}${HOME}${reset}."
 cd "$HOME"
@@ -45,13 +23,14 @@ mkdir -p .config/
 ln -sf ~/.dotfiles/i3/ .config/
 ln -sf ~/.dotfiles/polybar/ .config/
 ln -sf ~/.dotfiles/nvim/ .config/
+ln -sf ~/.dotfiles/fish/ .config/
 
-# Some applications need a different config on a different machine those are handled 
+# Some applications need a different config on a different machine those are handled
 # down here
 if [ "$(hostname)" = "fwarch" ]; then
-    echo -e "Using ${bold}fw-alacritty/${reset} because we're on the framework laptop."
-    ln -sfT ~/.dotfiles/fw-alacritty/ .config/alacritty
+	echo -e "Using ${bold}fw-alacritty/${reset} because we're on the framework laptop."
+	ln -sfT ~/.dotfiles/fw-alacritty/ .config/alacritty
 else
-    echo -e "Using ${bold}alacritty/${reset} because we're on a normal machine."
-    ln -sfT ~/.dotfiles/alacritty/ .config/
+	echo -e "Using ${bold}alacritty/${reset} because we're on a normal machine."
+	ln -sfT ~/.dotfiles/alacritty/ .config/
 fi
