a50d2c1747eef9aa5a7cd2ab1c25198414409f4d
Tim Fennis <tim.fennis@nos.nl>
2023-11-13 12:46:49 +0100

    don't create 0

--- .vimrc
+++ .vimrc
@@ -6,3 +6,4 @@ set nofixendofline
 set tabstop=4
 set shiftwidth=4
 set expandtab 
+set clipboard+=unnamed
--- fish/functions/gpf.fish
+++ fish/functions/gpf.fish
@@ -1,5 +1,5 @@
 function gpf
-    if count $argv >0
+    if count $argv
         git push -f origin $argv
     else
         git push -f origin (git branch --show-current)
