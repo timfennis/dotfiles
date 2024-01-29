0ba2bdbb7d95d61fab767a3364c47e627dad1b56
Tim Fennis <tim.fennis@nos.nl>
2024-01-29 10:21:39 +0100

    add starship

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
     set -g fish_greeting
+    starship init fish | source
 end
