2582411720ca66b5f624528ad14828877cfcbb9e
Tim Fennis <tim.fennis@nos.nl>
2024-03-19 12:56:05 +0100

    Use --force-with-lease in gpf alias

--- fish/functions/gpf.fish
+++ fish/functions/gpf.fish
@@ -1,7 +1,7 @@
 function gpf
     if set -q argv[1]
-        git push -f origin $argv
+        git push --force-with-lease origin $argv
     else
-        git push -f origin (git branch --show-current)
+        git push --force-with-lease origin (git branch --show-current)
     end
 end
